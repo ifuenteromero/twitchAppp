a8379da5e388778f5df36dbaf597a0df07bddbbd
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-10-28 16:27:09 +0100

    some basic components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-
-const PageOne = () => {
-    return <Link to="/pagetwo">PageOne</Link>
-}
-
-const PageTwo = () => {
-    return <Link to="/">PageTwo</Link>
-}
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
-        <div className="App">
+        <div className="ui container">
             <BrowserRouter>
-                <Route path="/" exact component={PageOne}/>
-                <Route path="/pagetwo" component={PageTwo} />
+                <Header />  
+                <Route path="/" exact component={StreamList}/>
+                <Route path="/streams/new" component={StreamCreate} />
+                <Route path="/streams/edit" component={StreamEdit} />
+                <Route path="/streams/delete" component={StreamDelete} />
+                <Route path="/streams/show" component={StreamShow} />
             </BrowserRouter>
         </div>
     );
